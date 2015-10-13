10c411d53262ffbdfafde17c21e3c2aa3e44d63d
Ivan-Roger <ivan.roger@iut2.upmf-grenoble.fr>
2015-10-13 15:12:02 +0200

    Base

---
+++ controler/afficherFlux.ctrl.php
@@ -0,0 +1,5 @@
+<?php
+//nothing yet
+$domDoc = new DOMDocument;
+$domDoc->load("http://www.lemonde.fr/m-actu/rss_full.xml");
+?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+  header("Location:"."controler/afficherFlux.ctrl.php")
+?>
